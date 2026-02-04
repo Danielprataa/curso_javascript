875854c0a9516ed43caad7e3b0c95062fbda1e1a
Daniel Moreira <danielmoreira@MacBook-Air-de-Daniel.local>
2026-02-04 18:07:12 -0300

    feat: aula-03

---
+++ Aula_03.js
@@ -0,0 +1,11 @@
+const idade = 8;
+  const nome = 'julio';
+  let podeentrar = false;
+if (idade <= 18) {
+    podeentrar = true;
+    console.log('ENTROU')
+}else {
+    console.log('NAO ENTRA!!!')
+}
+
+//console.log('Usuário pode entrar:', podeentrar);
