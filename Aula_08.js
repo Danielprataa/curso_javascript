ca5ec7775615b0ba64b0f5d8cd16cbd22546304d
Daniel Moreira <danielmoreira@MacBook-Air-de-Daniel.local>
2026-02-25 11:33:08 -0300

    feat: Aula-08

---
+++ Aula_08.js
@@ -0,0 +1,6 @@
+const salário = '103';
+const salárioNumero = Number(salário);
+
+console.log(' salário: ', salário);
+console.log(' salário numero: ', salárioNumero);
+console.log(' salário numero: ', salárioNumero.toFixed(2));
