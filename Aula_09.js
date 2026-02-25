a89c420196163b190de35a0dbe8f76310e6e6916
Daniel Moreira <danielmoreira@MacBook-Air-de-Daniel.local>
2026-02-25 11:47:58 -0300

    feat: aaula 09

---
+++ Aula_09.js
@@ -0,0 +1,12 @@
+function soma(item1, item2) {
+      return item1 + item2;
+}
+
+
+function diminuir(item1, item2) {
+     return item1 - item2;
+}
+
+console.log('soma: ', soma(10, 15));
+console.log('soma: ', soma(10, 25));
+console.log('diminuir: ', diminuir(10,5));
